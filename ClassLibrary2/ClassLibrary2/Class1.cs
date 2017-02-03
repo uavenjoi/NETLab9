f326d02b6552be96a9f80d99a58172aea9ddddd4
EkaterinaBulekova <tushka2030@mail.ru>
2017-02-03 22:54:23 +0600

    Не рабочий protected
    Не рабочий protected

---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_ClassLibrary
+{
+    public class Class1
+    {
+        public int A;
+        private int B;
+        protected int C;
+        internal int D;
+
+    }
+
+
+    public class Class2 : Class1
+    {
+        int i;
+        int = C;
+    }
+
+
+}
---
+++ ClassLibrary2/cons11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using My_ClassLibrary;
+
+namespace cons11
+{
+    class myClass: Class1
+    {
+        int i = C;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Class1 cl1 = new Class1();
+            myClass cl2 = new myClass();
+
+            i = cl1.A;
+            i = cl2.C;
+
+        }
+    }
+}
